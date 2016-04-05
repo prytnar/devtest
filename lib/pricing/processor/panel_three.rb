e55208f7a91760e41e1f277b54ca09f47fe6b4e1
Marek Bytnar <marek.bytnar@sabre.com>
2016-04-06 00:05:06 +0200

    panel 3 provider strategy implementation

--- lib/pricing/processor/panel_three.rb
+++ lib/pricing/processor/panel_three.rb
@@ -3,13 +3,18 @@ module Pricing
 		class PanelThree < Struct.new(:source)
 			delegate :read, to: :source
 
-
 			def price
-				0
+				get_all_nodes(Nokogiri::HTML(read)).size / 100
 			end
 
 			private
-
+			# I've used method described here:
+			# http://stackoverflow.com/questions/10076190/nokogiri-recursively-get-all-children
+			def get_all_nodes(xo)
+				result = []
+				xo.traverse {|node| result << node }
+				result
+			end
 		end
 	end
 end
